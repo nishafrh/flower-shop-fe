2a4a480e1d3978d13d5f7a2f74e49ddc374ed1ad
Nisha <nishafarah24.2@gmail.com>
2025-01-19 14:11:08 +0700

    Index Main Section

---
+++ src/views/home/mainSection/index.jsx
@@ -0,0 +1,54 @@
+import { Grid } from '@mui/material'
+
+import TitleGrid from './TitleGrid'
+import FlowersGrid from './FlowersGrid'
+import flower1 from '../../../assets/mainSection/flower-1.svg'
+import flower2 from '../../../assets/mainSection/flower-2.png'
+import flower3 from '../../../assets/mainSection/flower-3.png'
+import candels from '../../../assets/mainSection/candels.png'
+import fresheners from '../../../assets/mainSection/fresheners.png'
+
+const MainSection = () => (
+	<Grid container item xs={12} sx={{ border: '1px solid #121212' }}>
+		<TitleGrid />
+		<FlowersGrid
+			flowers={[
+				{
+					key: 'has-bottom',
+					title: 'Fresh Flowers',
+					img: flower1,
+					imgAlt: 'fresh-flower',
+					title2: 'Dried Flowers',
+					img2: flower2,
+					imgAlt2: 'dried-flower'
+				}
+			]}
+		/>
+		<Grid item xs={6} />
+		<FlowersGrid
+			flowers={[
+				{
+					key: 'has-bottom',
+					title: 'Live Plants',
+					img: flower3,
+					imgAlt: 'live-plants',
+					title2: 'Aroma Candels',
+					img2: candels,
+					imgAlt2: 'aroma-candels'
+				}
+			]}
+		/>
+		<Grid item xs={6} />
+		<FlowersGrid
+			flowers={[
+				{
+					title: 'Fresheners',
+					img: fresheners,
+					imgAlt: 'fresheners'
+				}
+			]}
+		/>
+	</Grid>
+)
+
+export default MainSection
