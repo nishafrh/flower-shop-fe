23adad4ae4220a5a86ec7880aaed4f3845e5b489
Nisha <nishafarah24.2@gmail.com>
2025-01-19 14:11:18 +0700

    Index Home Page

---
+++ src/views/home/index.jsx
@@ -0,0 +1,12 @@
+import { Grid } from '@mui/material'
+import MainSection from './mainSection'
+
+function HomePage() {
+	return (
+		<Grid container>
+			<MainSection />
+		</Grid>
+	)
+}
+
+export default HomePage
